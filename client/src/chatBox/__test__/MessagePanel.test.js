e69218c2543d3a6edd665629dd1dc842c94e329a
jonasps <jonas@gooddigitalmorning.com>
2018-11-11 22:57:49 +0100

    test for message rendering in MessagePanel

--- client/src/chatBox/__test__/MessagePanel.test.js
+++ client/src/chatBox/__test__/MessagePanel.test.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// *** configure Enzyme can be put in own file
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+// *** end configure
+
 import { MessagePanel } from "../MessagePanel";
 
 describe("MessagePanel", () => {
@@ -13,14 +20,24 @@ describe("MessagePanel", () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  // TODO: mock messages in state and test
-  // it("renders all messages saved in state", () => {
-  //   const div = document.createElement("div");
-  //   ReactDOM.render(<MessagePanel socket={mockSocket} />, div);
-  //
-  //   const numMessages = div.querySelectorAll("[data-class='message-in-chat']");
-  //   console.log(numMessages.length);
-  //
-  //   ReactDOM.unmountComponentAtNode(div);
-  // });
+  it("renders all messages in state", () => {
+    //
+    const messagePanel = shallow(<MessagePanel socket={mockSocket} />);
+    const exampleMessagesList = [
+      { message: "one" },
+      { message: "two" },
+      { message: "three" }
+    ];
+    messagePanel.setState(
+      {
+        messages: exampleMessagesList
+      },
+      () => {
+        const numOfFoundMessages = messagePanel.find(
+          "[data-class='message-in-chat']"
+        ).length;
+        expect(numOfFoundMessages).toBe(exampleMessagesList.length);
+      }
+    );
+  });
 });
